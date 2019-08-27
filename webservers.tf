e5824dc01af11f9dc6182c1a75ec10151d0eb98b
Thomas Beaumont <tombeaumont@Thomass-iMac-2.local>
2019-08-27 15:41:31 +0200

    test apache

--- webservers.tf
+++ webservers.tf
@@ -8,11 +8,21 @@ resource "aws_network_interface" "web1-int" {
 
 data "template_file" "config" {
   template = <<EOF
-     #!/bin/bash
-     echo Webserver1 > index.html && nohup busybox httpd -f -p 80
-     until $(curl -sSL -k --output /dev/null --silent --head --fail https://jenkins.minimal.net.au:8083); do printf '.' sleep 5 done
-     curl -sSL -k --header "authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGhiZWF1bW9udEBwYWxvYWx0b25ldHdvcmtzLmNvbSIsInJvbGUiOiJhZG1pbiIsImdyb3VwcyI6bnVsbCwicHJvamVjdHMiOm51bGwsInNlc3Npb25UaW1lb3V0U2VjIjo4NjQwMCwiZXhwIjoxNTY2OTg4NTU3LCJpc3MiOiJ0d2lzdGxvY2sifQ.aAUW-AdIUJU3y1g3-Y3s6jW574rmpKfHsG9NXC01v_Q" https://jenkins.minimal.net.au:8083/api/v1/scripts/defender.sh | sudo bash -s -- -c "jenkins.minimal.net.au" -d "none"  --install-host
-  EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "Webserver1" > /var/www/html/phpinfo.php
+until $(curl -sSL -k --output /dev/null --silent --head --fail https://jenkins.minimal.net.au:8083); do printf '.' sleep 5 done
+curl -sSL -k --header "authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGhiZWF1bW9udEBwYWxvYWx0b25ldHdvcmtzLmNvbSIsInJvbGUiOiJhZG1pbiIsImdyb3VwcyI6bnVsbCwicHJvamVjdHMiOm51bGwsInNlc3Npb25UaW1lb3V0U2VjIjo4NjQwMCwiZXhwIjoxNTY2OTg4NTU3LCJpc3MiOiJ0d2lzdGxvY2sifQ.aAUW-AdIUJU3y1g3-Y3s6jW574rmpKfHsG9NXC01v_Q" https://jenkins.minimal.net.au:8083/api/v1/scripts/defender.sh | sudo bash -s -- -c "jenkins.minimal.net.au" -d "none"  --install-host
+EOF
 }
 resource "aws_instance" "web1" {
   instance_initiated_shutdown_behavior = "stop"
@@ -59,5 +69,5 @@ resource "aws_instance" "web2" {
   }
 
   user_data = "${data.template_file.config.rendered}"
-  
+
 }
