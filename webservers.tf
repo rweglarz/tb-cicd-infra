3c96f273b5fa899f3a1bf2c4d72d71442a9adf32
Thomas Beaumont <tombeaumont@Thomass-iMac-2.local>
2019-08-27 12:38:44 +0200

    with defender install

--- webservers.tf
+++ webservers.tf
@@ -23,7 +23,8 @@ resource "aws_instance" "web1" {
 
   user_data = "${base64encode(join("", list(
    "#! /bin/bash\n",
-           "echo Webserver1 > index.html && nohup busybox httpd -f -p 80"
+           "echo Webserver1 > index.html && nohup busybox httpd -f -p 80",
+           "curl -sSL -k --header "authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGhiZWF1bW9udEBwYWxvYWx0b25ldHdvcmtzLmNvbSIsInJvbGUiOiJhZG1pbiIsImdyb3VwcyI6bnVsbCwicHJvamVjdHMiOm51bGwsInNlc3Npb25UaW1lb3V0U2VjIjo4NjQwMCwiZXhwIjoxNTY2OTg4NTU3LCJpc3MiOiJ0d2lzdGxvY2sifQ.aAUW-AdIUJU3y1g3-Y3s6jW574rmpKfHsG9NXC01v_Q" https://jenkins.minimal.net.au:8083/api/v1/scripts/defender.sh | sudo bash -s -- -c "jenkins.minimal.net.au" -d "none"  --install-host"
    )))
    }"
 }
@@ -55,7 +56,8 @@ resource "aws_instance" "web2" {
 
   user_data = "${base64encode(join("", list(
     "#! /bin/bash\n",
-            "echo Webserver2 > index.html && nohup busybox httpd -f -p 80"
+            "echo Webserver2 > index.html && nohup busybox httpd -f -p 80",
+             "curl -sSL -k --header "authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGhiZWF1bW9udEBwYWxvYWx0b25ldHdvcmtzLmNvbSIsInJvbGUiOiJhZG1pbiIsImdyb3VwcyI6bnVsbCwicHJvamVjdHMiOm51bGwsInNlc3Npb25UaW1lb3V0U2VjIjo4NjQwMCwiZXhwIjoxNTY2OTg4NTU3LCJpc3MiOiJ0d2lzdGxvY2sifQ.aAUW-AdIUJU3y1g3-Y3s6jW574rmpKfHsG9NXC01v_Q" https://jenkins.minimal.net.au:8083/api/v1/scripts/defender.sh | sudo bash -s -- -c "jenkins.minimal.net.au" -d "none"  --install-host"
     )))
     }"
 }
